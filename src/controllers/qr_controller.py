c0649a2dcbbf1cc91b7b09e6a637b5f0f8a3eaa3
Abraham <abrixa12@gmail.com>
2024-06-11 14:01:34 -0500

    New funcionaliti

--- src/controllers/qr_controller.py
+++ src/controllers/qr_controller.py
@@ -1,8 +1,14 @@
+import base64
 from io import BytesIO
 from src.instances.qr_instance import Run
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.colormasks import RadialGradiantColorMask
 from fastapi.responses import StreamingResponse
+from fastapi import UploadFile
+
+
+def ResponseQr():
+    pass
 
 
 class QrController:
@@ -24,6 +30,72 @@ class QrController:
         return StreamingResponse(img_bytes_io, media_type="image/png")
 
     @staticmethod
-    async def Email():
-        pass
+    async def Email(to: str, sub: str, body: str):
+        qr = Run()
+        qr.add_data(f"MATMSG:TO:{to};SUB:{sub};BODY:{body};;")
 
+        img = qr.make_image(
+            image_factory=StyledPilImage,
+            color_mask=RadialGradiantColorMask()
+        )
+
+        img_bytes_io = BytesIO()
+        img.save(img_bytes_io, format='PNG')
+        img_bytes_io.seek(0)
+
+        return StreamingResponse(img_bytes_io, media_type="image/png")
+
+    @staticmethod
+    async def Text(message: str):
+        qr = Run()
+        qr.add_data(message)
+
+        img = qr.make_image(
+            image_factory=StyledPilImage,
+            color_mask=RadialGradiantColorMask()
+        )
+
+        img_bytes_io = BytesIO()
+        img.save(img_bytes_io, format='PNG')
+        img_bytes_io.seek(0)
+
+        return StreamingResponse(img_bytes_io, media_type="image/png")
+
+    @staticmethod
+    async def Whatsapp(phone: int, message: str):
+        qr = Run()
+        qr.add_data(f"https://wa.me/{phone}/?text={message}")
+
+        img = qr.make_image(
+            image_factory=StyledPilImage,
+            color_mask=RadialGradiantColorMask()
+        )
+
+        img_bytes_io = BytesIO()
+        img.save(img_bytes_io, format='PNG')
+        img_bytes_io.seek(0)
+
+        return StreamingResponse(img_bytes_io, media_type="image/png")
+
+    @staticmethod
+    async def Image(file: UploadFile):
+        image_bytes = await file.read()
+
+        image_base64 = base64.b64encode(image_bytes).decode()
+
+        qr_content = f"data:image/jpeg;base64,{image_base64}"
+
+        qr = Run()
+        qr.add_data(qr_content)
+        qr.make(fit=True)
+
+        img = qr.make_image(
+            image_factory=StyledPilImage,
+            color_mask=RadialGradiantColorMask()
+        )
+
+        img_bytes_io = BytesIO()
+        img.save(img_bytes_io, format='PNG')
+        img_bytes_io.seek(0)
+
+        return StreamingResponse(img_bytes_io, media_type="image/png")
--- src/v1/routes/qr_route.py
+++ src/v1/routes/qr_route.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, File, UploadFile
 from src.controllers.qr_controller import QrController
 
 router = APIRouter()
@@ -10,5 +10,20 @@ async def link(website: str):
 
 
 @router.get("/email")
-async def email():
-    return await QrController.Email()
+async def email(to: str, sub: str, body: str):
+    return await QrController.Email(to, sub, body)
+
+
+@router.get("/text")
+async def text(message: str):
+    return await QrController.Text(message)
+
+
+@router.get("/whatsapp")
+async def whatsapp(phone: int, message: str):
+    return await QrController.Whatsapp(phone, message)
+
+
+@router.get("/image")
+async def image(file: UploadFile = File(...)):
+    return await QrController.Image(file)
